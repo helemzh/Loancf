10232b631af5da64b44c13b2a0c23345e3bbb8d9
helemzh <hajzhang@gmail.com>
2024-11-24 20:37:34 -0500

    smm, amortization, and vectorized functions

--- main.py
+++ main.py
@@ -1,43 +1,33 @@
 import numpy_financial as npf
 import pandas as pd
+import numpy as np
 
-rate = 0.06 / 12
-nper = 360
+wam = 24 # months
+wala = wam + 1
+wac = 0.06
+rate = wac / 12
 pv = 100000
 
 # Fixed monthly payment
-X = npf.pmt(rate, nper, pv)
+X = npf.pmt(rate, wam, pv)
 
 # Initialize lists for each row of data
-months = list(range(nper + 1))
-balances = [pv]
-rates = [.06] * (nper + 1)
-payments = [abs(X)] * (nper + 1)
-interests = []
-principals = []
+months = np.arange(wala)
+balances =  pv * (1 - (1 + wac/12.) ** -(wam - np.arange(wala))) / (1 - (1+wac/12.)**-wam)
+rates = np.ones(wala) * wac
+payments = np.ones(wala) * abs(X)
 
-# Generate the payment schedule
-balance = pv
-for month in range(1, nper + 1):
-    interest = balance * rate
-    principal = abs(X) - interest
-    balance -= principal
+interests = np.zeros_like(balances)
+interests[1:] = balances[:-1] * rate
 
-    # Append values to lists
-    if balance > 0:
-        balances.append(balance)
-    else:
-        balances.append(0)
-    interests.append(interest)
-    principals.append(principal)
-    
-# Insert a zero interest and principal for the initial month
-interests.insert(0, 0)
-principals.insert(0, 0)
+principals = abs(X) - interests
+
+interests[0] = 0
+principals[0] = 0
 payments[0] = 0
 
-# Create a DataFrame
-df = pd.DataFrame({
+# Mortgage Amortization
+amortization = pd.DataFrame({
     "Month": months,
     "Balance": balances,
     "Mortgage Rate (WAC)": rates,
@@ -46,4 +36,41 @@ df = pd.DataFrame({
     "Principal": principals
 })
 
-print(df)
+#smm/survivorship
+smmVector = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02]
+survivorship = np.ones(wala)
+survivorship = np.cumprod(survivorship * (np.ones(wala) - smmVector))
+
+balancesTemp = np.zeros(wala)
+balancesTemp[:-1] = balances[1:]
+actualBalance = survivorship * balancesTemp
+
+survivorshipTemp = np.ones(wala)
+survivorshipTemp[1:] = survivorship[:-1]
+
+beginningBalance = np.ones(wala) * 100000
+beginningBalance[1:] = actualBalance[:-1]
+
+prepayPrin = survivorshipTemp * balancesTemp * smmVector
+
+amortPrinTemp = np.ones(wala)
+amortPrinTemp[:-1] = principals[1:]
+schedPrin = survivorshipTemp * amortPrinTemp
+
+balanceCheck = beginningBalance - actualBalance
+
+actInterest = beginningBalance * rate
+
+smm = pd.DataFrame({
+    "smm vector": smmVector,
+    "survivorship": survivorship,
+    "b_bal": beginningBalance,
+    "actual balance": actualBalance,
+    "prepay prin": prepayPrin,
+    "sched prin": schedPrin,
+    "balance chk": balanceCheck,
+    "actual interest": actInterest
+})
+
+# print(amortization)
+print(smm)
