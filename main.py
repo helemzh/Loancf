08ec6394083c6f1d3494a7caffd97fe1a938ee4e
helemzh <hajzhang@gmail.com>
2024-12-02 11:55:33 -0500

    added if name == main

--- main.py
+++ main.py
@@ -121,23 +121,24 @@ class Output:
         return self.resultPX
     
 
-loan = Loan(wac=0.0632, wam=357, pv=100000000)
-# Define vectors for the scenario
-smmVec = np.ones(loan.wam) * 0.01
-dqVec = np.ones(loan.wam) * 0.1
-mdrVec = np.ones(loan.wam) * 0.1
-sevVec = np.ones(loan.wam) * 0.2
+if __name__ == '__main__':
+    loan = Loan(wac=0.0632, wam=357, pv=100000000)
+    # Define vectors for the scenario
+    smmVec = np.ones(loan.wam) * 0.01
+    dqVec = np.ones(loan.wam) * 0.1
+    mdrVec = np.ones(loan.wam) * 0.1
+    sevVec = np.ones(loan.wam) * 0.2
 
 
-scenario = Scenario(smmV=smmVec, dqV=dqVec, mdrV=mdrVec, sevV=sevVec)
-y = Yield(yieldValue=0.055)
-x = Yield(yieldValue=0.0632)
+    scenario = Scenario(smmV=smmVec, dqV=dqVec, mdrV=mdrVec, sevV=sevVec)
+    y = Yield(yieldValue=0.055)
+    x = Yield(yieldValue=0.0632)
 
-output = Output(loan=loan, scenario=scenario, px=y)
-output1 = Output(loan=loan, scenario=scenario, px=x)
+    output = Output(loan=loan, scenario=scenario, px=y)
+    output1 = Output(loan=loan, scenario=scenario, px=x)
 
-df = output.getCashflow()
-px = output.getPX()
+    df = output.getCashflow()
+    px = output.getPX()
 
-print(df)
-print(px)
+    print(df)
+    print(px)
